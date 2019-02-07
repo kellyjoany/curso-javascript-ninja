a94b31973d6e9158cea8a268bd52dc7fabe5a429
kellyjoany <45659063+kellyjoany@users.noreply.github.com>
2019-02-07 20:35:41 -0300

    Update challenge-05.js

--- challenge-05/challenge-05.js
+++ challenge-05/challenge-05.js
@@ -2,30 +2,33 @@
 Crie uma variável qualquer, que receba um array com alguns valores aleatórios
 - ao menos 5 - (fica por sua conta os valores do array).
 */
-// ?
+var dados = ["a", "b", "c", "d", "e"];
 
 /*
 Crie uma função que receba um array como parâmetro, e retorne esse array.
 */
-// ?
-
+var retorno = function(arg){
+  return arg;
+}
+retorno(dados);
 /*
 Imprima o segundo índice do array retornado pela função criada acima.
 */
-// ?
-
+retorno(dados[1]);
 /*
 Crie uma função que receba dois parâmetros: o primeiro, um array de valores; e o
 segundo, um número. A função deve retornar o valor de um índice do array que foi passado
 no primeiro parâmetro. O índice usado para retornar o valor, deve ser o número passado no
 segundo parâmetro.
 */
-// ?
+var retorno1 = function(arg, x){
+  return arg[x];
+}
 
 /*
 Declare uma variável que recebe um array com 5 valores, de tipos diferentes.
 */
-// ?
+var dados = ["a", 2, null, true, "kelly"];
 
 /*
 Invoque a função criada acima, fazendo-a retornar todos os valores do último
